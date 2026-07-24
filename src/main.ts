8b075a9afd9a01a7a93c8b960bf25b6683b894a8
munteanudenis87 <munteanudenis87@gmail.com>
2026-07-24 20:27:09 +0200

    Snack 2

--- src/main.ts
+++ src/main.ts
@@ -31,4 +31,27 @@ if(typeof dato === 'string'){
   console.log('Tipo non supportato');
 }
 
+//  Snack 2
+//  Crea un type alias Dipendente che rappresenta un lavoratore con i seguenti dati:
 
+//  nome → stringa
+//  cognome → stringa
+//  annoNascita → numero
+//  sesso → Può essere solo "m" o "f".
+//  anniDiServizio (array di numeri, es. [2014, 2015, 2017, 2018])
+
+//  BONUS
+//  Il type alias Dipendente, ha anche i seguenti dati:
+
+//  emailAziendale → Email assegnata al dipendente (non si può modificare)
+//  contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”.
+
+type Dipendente = {
+  nome: string,
+  cognome: string,
+  annoNascita: number,
+  sesso: 'm' | 'f',
+  anniDiServizio: number[],
+  readonly emailAziendale: string,
+  contratto: 'indeterminato' | 'determinato' | 'freelance'
+}

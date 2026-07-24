1f255c27892420d9c92bdb4c05220c688791ca63
munteanudenis87 <munteanudenis87@gmail.com>
2026-07-24 20:43:31 +0200

    Snack 3

--- src/main.ts
+++ src/main.ts
@@ -55,3 +55,41 @@ type Dipendente = {
   readonly emailAziendale: string,
   contratto: 'indeterminato' | 'determinato' | 'freelance'
 }
+
+//  Snack 3
+//  Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+
+//  Developer
+//  livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+//  linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+//  certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+//  ProjectManager
+//  teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+//  budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+//  stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
+//  🎯 BONUS
+//  Definiamo un nuovo type alias Team, che rappresenta un gruppo di lavoro all'interno dell'azienda:
+
+//  nome → Nome del team (stringa).
+//  progettoAttuale → Nome del progetto su cui lavora il team (può essere null se il team è in attesa di assegnazione).
+//  budget → Importo numerico del budget assegnato al team (sempre presente).
+//  membri → Una tuple in cui il primo elemento è sempre un Project Manager, seguito da uno o più Developers (almeno un developer obbligatorio).
+
+type Developer = Dipendente & {
+  livelloEsperienza: 'Junior' | 'Mid' | 'Senior',
+  linguaggi?: string[], 
+  certificazioni: string[],
+}
+
+type ProjectManager = {
+  teamSize: number | null,
+  budgetGestito?: number,
+  stakeholderPrincipali: string[],
+}
+
+type Team = {
+  nome: string,
+  progettoAttuale: string | null,
+  budget: number,
+  membri: [ProjectManager, Developer, ...Developer[]],
+}
